958b2c34a6a465ed9f207e74e2fb6f3acc658064
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-01-15 10:43:37 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,6 @@
+USE firstdatabase;
+INSERT INTO planeten (naam) VALUES("Zon");
+INSERT INTO planeten (naam) VALUES("Mercurius");
+INSERT INTO planeten (naam) VALUES("Venus");
+INSERT INTO planeten (naam) VALUES("Aarde");
+INSERT INTO planeten (naam) VALUES("Marsnaam");
